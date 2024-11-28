b642f1c6c0e93d7af39c181f3e13c1f7eae4d707
muralaswarupa <159383944+muralaswarupa@users.noreply.github.com>
2024-11-28 17:14:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def get_connection():
         database="diabetes_db",
         user="postgres",
         password="swarupa",
-        host="4.tcp.ngrok.io",         # Replace with the ngrok public host
-        port="17547"  
+        host="2.tcp.ngrok.io",         # Use the new ngrok public host
+        port="13601"   
     )
 
 def run_query(query):
